c64a6ad5c826823fdb43b3740dc9b95fe73b6e5a
One Dionys <161098412+onedionys@users.noreply.github.com>
2024-03-17 20:35:40 +0700

    chore: initialize function project

---
+++ src/formWizard.js
@@ -0,0 +1,33 @@
+class FormWizard {
+    constructor() {
+        // Implementasi konstruktor
+    }
+
+    addStep(stepName, stepContent) {
+        // Tambahkan langkah ke dalam wizard
+    }
+
+    removeStep(stepName) {
+        // Hapus langkah dari wizard
+    }
+
+    goToStep(stepName) {
+        // Pergi ke langkah tertentu dalam wizard
+    }
+
+    nextStep() {
+        // Pindah ke langkah berikutnya dalam wizard
+    }
+
+    prevStep() {
+        // Kembali ke langkah sebelumnya dalam wizard
+    }
+
+    getCurrentStep() {
+        // Dapatkan langkah saat ini dalam wizard
+    }
+
+    // Metode lainnya sesuai kebutuhan
+}
+
+module.exports = FormWizard;
